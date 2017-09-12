cba9433323817bc09303d6a11e85afe111316bf7
Jon Steinich <jons@perblue.com>
2017-09-12 09:09:35 -0500

    Wait for patches to load async components before marking as loaded

--- core/src/net/mgsx/pd/patch/PatchLoader.java
+++ core/src/net/mgsx/pd/patch/PatchLoader.java
@@ -1,27 +1,54 @@
 package net.mgsx.pd.patch;
 
-import com.badlogic.gdx.assets.AssetDescriptor;
-import com.badlogic.gdx.assets.AssetLoaderParameters;
-import com.badlogic.gdx.assets.AssetManager;
+import java.util.concurrent.CountDownLatch;
+
+import org.puredata.core.PdListener;
+
+import com.badlogic.gdx.assets.*;
 import com.badlogic.gdx.assets.loaders.AsynchronousAssetLoader;
 import com.badlogic.gdx.assets.loaders.FileHandleResolver;
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Array;
 
 import net.mgsx.pd.Pd;
+import net.mgsx.pd.utils.PdRuntimeException;
 
 public class PatchLoader extends AsynchronousAssetLoader<PdPatch, AssetLoaderParameters<PdPatch>>
 {
 	private PdPatch patch;
+	private boolean asyncPatches;
+	private volatile int loadingPatch;
+	private volatile CountDownLatch loadingLatch;
 	
-	public PatchLoader(FileHandleResolver resolver) {
+	public PatchLoader(FileHandleResolver resolver, boolean enableAsync) {
 		super(resolver);
+		asyncPatches = enableAsync;
+		if(enableAsync) {
+			Pd.audio.addListener("patchLoaded", new PdListener.Adapter() {
+				@Override
+				public void receiveFloat(String source, float x) {
+					if(MathUtils.isEqual(loadingPatch, x)) {
+						loadingLatch.countDown();
+					}
+				}
+			});
+		}
 	}
 
 	@Override
 	public void loadAsync(AssetManager manager, String fileName, FileHandle file,
 			AssetLoaderParameters<PdPatch> parameter) {
 		patch = Pd.audio.open(file);
+		if(asyncPatches) {
+			loadingPatch = patch.getPdHandle();
+			loadingLatch = new CountDownLatch(1);
+			try {
+				loadingLatch.await();
+			} catch (InterruptedException e) {
+				throw new PdRuntimeException("Error waiting for patch load", e);
+			}
+		}
 	}
 
 	@Override
